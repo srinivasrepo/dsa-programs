af5aee7c4df8d4d39066f36b57f61113c9a03dcd
Srinivas Vadige <srinivas.vadige@gmail.com>
2024-06-09 02:04:35 -0600

    General Package deleted

--- TestRecord.java
+++
@@ -1,9 +0,0 @@
-public record TestRecord(int foo, String bar) {
-    TestRecord setFoo(int newFoo){
-        return new TestRecord(newFoo,  bar); 
-    }
-    TestRecord setbar(String newBar){
-        return new TestRecord(foo,  newBar); 
-    }
-    public TestRecord{}
-}
