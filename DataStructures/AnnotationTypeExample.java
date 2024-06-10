4ab881603a29bc1b144e1d3ef98e11ba76e0afc9
Srinivas Vadige <srinivas.vadige@gmail.com>
2024-06-09 19:37:23 -0600

    AnnotationType usecase and explanation in AnotherClass someMethod()

--- DataStructures/AnnotationTypeExample.java
+++ DataStructures/AnnotationTypeExample.java
@@ -1,15 +1,27 @@
 package DataStructures;
 
-/* 
-In annotation type interface we can only use methods with return type as primitives, String, Array of primitives. 
-We cannot use Non-primitives or classes or list.
- */
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.annotation.ElementType;
 import java. lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java. lang. annotation.Target;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+/* 
+In annotation type interface we can only use methods with return type as primitives, String, Array of primitives. 
+We cannot use Non-primitives or classes or list.
+ */
 
-
+/* Annotation Types basically don’t change our code in real-time but will helpful in the runtime. Annotations can be class level @Path, method level @Override or variable level @SuppressWarnings. And will defining our custom annotations we use some in-built or another custom annotations. This custom annotation’s in-built annotations are 
+1) @Target(“ClassMethodVarLevel”) - optional and leave it if you want to use this for all levels. Inside value is like ElementType.METHOD, .TYPE(class), .PACKAGE., .CONSTRUCTOR, .FIELD(variable)
+2) @Retention(RetentionPolicy.RUNTIME) to keep this annotation alive up to what extent i.e annotation life cycle. RUNTIME means alive all the way through runtime, so that other code can use this annotation. Other values like RetentionPolicy.SOURCE means make alive even before the code is complied like SuppressWarning annotation, RetentionPolicy.CLASS means only active up to compile and destroys at runtime.
+3) @Documented...... 
+*/
 @Retention(RetentionPolicy.RUNTIME) // optional
 @Target({ElementType.METHOD, ElementType.FIELD}) // optional
 public @interface AnnotationTypeExample {
@@ -25,6 +37,11 @@ public @interface AnnotationTypeExample {
         return "test";  // i.e we can only have default value but not the default method in @interface
     } */
 }
+
+@interface RunImmediately{
+    int times() default 1;
+}
+
 /* 
 which can then annotate a class as follows:
 @AnnotationTypeExample (       //The annotation @AnnotationTypeExample is disallowed for this location or same file
@@ -36,28 +53,80 @@ which can then annotate a class as follows:
    // Note array notation
    reviewers = {"Alice", "Bob", "Cindy"}
 )
-public class Generation3List extends Generation {
-      // class code goes here
-}
 */
+class ClassWithAnnotation {
+      // class code goes here
+
+      // @RunImmediately
+      void classMethodWithAnnotation(){
+
+      }
+}
 
 
 
 /*
-Annotation Types basically don’t change our code in real-time but will helpful in the runtime. Annotations can be class level @Path, method level @Override or variable level @SuppressWarnings. And will defining our custom annotations we use some in-built or another custom annotations. This custom annotation’s in-built annotations are 
-1) @Target(“ClassMethodVarLevel”) - optional and leave it if you want to use this for all levels. Inside value is like ElementType.METHOD, .TYPE(class), .PACKAGE., .CONSTRUCTOR, .FIELD(variable)
-2) @Retention(RetentionPolicy.RUNTIME) to keep this annotation alive up to what extent i.e annotation life cycle. RUNTIME means alive all the way through runtime, so that other code can use this annotation. Other values like RetentionPolicy.SOURCE means make alive even before the code is complied like SuppressWarning annotation, RetentionPolicy.CLASS means only active up to compile and destroys at runtime.
-3) @Documented......
+Get 
 We can check whether a class is annotated or not by
 myClass.getClass().isAnnotationPresent(CustomAnnotation.class);
 That is how all the project classes are looped to check for @Configuration annotations classes and again each configClass is looped with all it’s methods to check the @Bean methods then the Spring executes all the beans methods (using Method.invoke()) when our Spring application starts running.
 We can pass arguments to custom annotations. We know that interface can only have static and constant variables. So, make them as methods but can be used in @RunImmediately(times=3)
 for (Method method : myCat.getclass.getDeclaredMethods()){
     if (method.isAnnotationPresent(RunImmediately.class)){
-        RunImmediately annotation = method.getAnnotation(RunImmediately.c
+        RunImmediately annotation = method.getAnnotation(RunImmediately.class)
         for (int i = 0; i ‹ annotation.times); i++){ method. invoke(myCat); }
     }
 } 
-    
-
 */
+class AnotherClass {
+
+    static void someMethod() throws InvocationTargetException, IllegalAccessException { // or Exception
+
+        // get List of all packages in our project
+        String packageNamePattern = "com.mypack.";
+        List<String> packagesList = Arrays.stream(Package.getPackages())
+                                        .map(Package::getName)
+                                        .filter(n -> n.startsWith(packageNamePattern))
+                                        .toList();
+        
+        // get List of all classes from a single packege
+        String packageName = packagesList.get(0);
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                                        .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        @SuppressWarnings("rawtypes")
+        List<Class> setOfClasses = reader.lines()
+                .filter(line -> line.endsWith(".class"))
+                .map(line -> getClass(line, packageName))
+                .collect(Collectors.toList());
+        try {
+            setOfClasses.get(0).getClassLoader().loadClass(setOfClasses.get(0).getName());
+        } catch (ClassNotFoundException e) {
+        }
+
+        // validate with our custom annotaions
+        ClassWithAnnotation myClass = new ClassWithAnnotation();
+        myClass.getClass().isAnnotationPresent(AnnotationTypeExample.class); // same like @Configuration
+
+        for (Method method : myClass.getClass().getDeclaredMethods()){
+            if (method.isAnnotationPresent(AnnotationTypeExample.class)) {
+                RunImmediately annotation = method.getAnnotation(RunImmediately.class);  // same like @Bean
+                for (int i = 0; i < annotation.times(); i++)
+                    method.invoke(new Object());
+            }
+        } 
+
+    }
+
+    @SuppressWarnings("rawtypes")
+    static Class getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "."
+              + className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException e) {
+            // handle the exception
+        }
+        return null;
+    }
+}
+
