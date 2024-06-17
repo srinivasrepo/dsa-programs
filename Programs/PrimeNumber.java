c83d831dd0503de87bd72acdb07cb9db3b4d59b6
Srinivas Vadige <srinivas.vadige@gmail.com>
2024-06-16 18:16:21 -0600

    added more explantion

--- Programs/PrimeNumber.java
+++ Programs/PrimeNumber.java
@@ -2,17 +2,67 @@ package Programs;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        System.out.println(isPrime(40)? "yes": "no");
+        int n = 41;
+        System.out.println("Brute force => " + (isPrimeBruteForce(n) ? "yes" : "no"));
+        System.out.println("Better approach or Half loop with count => " + (isPrimeHalfLoopApproachWithCount(n) ? "yes" : "no"));
+        System.out.println("Better approach or Half loop without count => " + (isPrimeHalfLoopApproach(n) ? "yes" : "no"));
     }
 
-    static boolean isPrime(int num) {
+    static boolean isPrimeBruteForce(int num) {
         // Corner case as 1 is not prime and 0 & negatives are also not prime
         if (num <= 1)
             return false;
         // Check from 2 to n-1
-        for (int i = 2; i < num; i++) // or  for (int i = 2; i <= n / 2; i++) or for (int i = 2; i <= Math.sqrt(n); i++)
+        for (int i = 2; i < num; i++)
             if (num % i == 0)
                 return false;
         return true;
     }
+
+    /*
+     * 
+     * or loop untill i <= (int) Math.sqrt(num); or i <= num/2 with
+     * Add the counterpart divisor if it's different from i
+     * => if (i != num / i) divisors.add(num / i);
+     * 
+     * cause
+     * the divisors of 36 number are [1, 2, 3, 4, 6, 9, 12, 18, 36]
+     * 1*36
+     * 2*18
+     * 3*12
+     * 4*9
+     * 6*6
+     * 9*4
+     * 12*3
+     * 18*2
+     * 36*1
+     * 
+     * this is repeating after 6*6 in reverse order
+     */
+
+    static boolean isPrimeHalfLoopApproachWithCount(int n) {        
+        if (n <= 1) return false; // Corner case as 1 is not prime and 0 & negatives are also not prime
+
+        int cnt = 0; // Initialize a counter variable to count the number of factors.
+
+        for (int i = 2; i <= Math.sqrt(n); i++) { // or for (int i = 2; i <= n/2; i++)
+            if (n % i == 0) { // If n is divisible by i without any remainder.
+                cnt = cnt + 1; // Increment the counter.
+                
+                if (n / i != i) cnt = cnt + 1; // OPTIONAL : If n is not a perfect square, count its reciprocal factor.
+            }
+        }
+        
+        if (cnt > 2) // If the number of factors is exactly 2.
+            return true;
+        return false;
+    }
+
+    static boolean isPrimeHalfLoopApproach(int n) {
+        for (int i = 1; i <= Math.sqrt(n); i++) { // or for (int i = 2; i <= n/2; i++)
+            if(n%i == 0) return true;
+        }
+        return false;
+    }
+
 }
--- Programs/ReverseNumber.java
+++ Programs/ReverseNumber.java
@@ -4,23 +4,46 @@ public class ReverseNumber {
 
     public static void main(String[] args) {
         int n = 4526; 
-        System.out.print("Reversed Number is " + reverse(n)); 
+        System.out.println("USING DIV /10 AND MODULS %10 APPROACH => " + reverse(n));
+        System.out.println("USING STRING REVERSE => " + stringReverse(n)); 
+        System.out.println("USING STRING BUILDER REVERSE => " + stringBuilderReverse(n)); 
     }
 
     // reversing using %10 and /10 concept
     static int reverse(int n) { 
+
         // reversed number 
         int rev = 0; 
-        // remainder 
-        int rem; 
   
         while (n > 0) { 
-            rem = n % 10; 
-            rev = (rev * 10) + rem; 
-            n = n / 10; 
+        int lastDigit = n % 10; // remainder
+            rev = (rev * 10) + lastDigit; 
+            n = n / 10; // quotient
         } 
   
         return rev; 
-    } 
+    }
+
+
+    static int stringReverse(int n){
+        String oStr = String.valueOf(n);
+        String nStr = "";
+
+        for (char i : oStr.toCharArray()) { // or for index loop
+            nStr = i+nStr; // it is different from nStr += i
+        }
+
+        // // or 
+        // while (!oStr.equals("")) {
+        //     nStr += oStr.charAt(oStr.length()-1);
+        //     oStr = oStr.substring(0, oStr.length()-1);
+        // }
+
+        return Integer.parseInt(nStr);
+    }
+
+    static int stringBuilderReverse(int n){
+        return Integer.parseInt(new StringBuffer(String.valueOf(n)).reverse().toString());
+    }
     
 }
