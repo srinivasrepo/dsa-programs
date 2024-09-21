80db49218f1c96fa9685d8a4d9d411ab8ac3c39a
Srinivas Vadige <srinivas.vadige@gmail.com>
2024-09-21 14:16:00 -0600

    Updated getDivisorsWithSqrtLoop

--- BasicPrograms/AllDivisors.java
+++ BasicPrograms/AllDivisors.java
@@ -3,15 +3,17 @@ package BasicPrograms;
 import java.util.ArrayList;
 import java.util.List;
 
+// Divisors or factors are the numbers that can divide the given number i.e num%i == 0 i.e not remainder   
 public class AllDivisors {
     public static void main(String[] args) {
         int n = 36;
         System.out.println("sorted divisors brute force " + getDivisorsBruteForce(n));
-        System.out.println("sorted divisors with half Loop better approach " + getDivisorsWithHalfLoopBetterApproach(n));
+        System.out.println("sorted divisors with sqrt() approach " + getDivisorsWithSqrtLoop(n));
     }
 
     /* 
-    or loop untill i <= (int) Math.sqrt(n); or i <= n/2 with 
+    Note that i <= n/2 is bad approach for divisors, can use it for prime number
+    or loop untill i <= (int) Math.sqrt(n);
     Add the counterpart divisor if it's different from i
     => if (i != n / i) divisors.add(n / i);
 
@@ -40,18 +42,45 @@ public class AllDivisors {
         return divs;
 
     }
-    static List<Integer> getDivisorsWithHalfLoopBetterApproach(int n) {
+    static List<Integer> getDivisorsWithSqrtLoop(int n) { // Note that for just sysout if else conditions won't print in order. So, use this method to get divs list and print it
         List<Integer> divs = new ArrayList<>();
         for (int i = 1; i <= Math.sqrt(n); i++) {
             if (n%i == 0) {
-                divs.add(i); //unsort insertion [1, 36, 2, 18, 3, 12, 4, 9, 6] // or use sort insertion [1, 2, 3, 4, 6, 9, 12, 18, 36] with below logic
-                // if (divs.size()>1 && i < divs.get(divs.size()/2)) divs.add(divs.size()/2, i); else divs.add(i);
+                // NORMAL ---------------
+                if (divs.size() != 1) divs.add(divs.size()/2, i); else divs.add(i); // corner-case for 1
+                ////PATTERM=> [] i=1 | [1,36] i=2 | [1,2,18,26] i=3 -- size is even
 
+                // COUNTERPART ---------------
                 int iMutiplier = n/i;
-                if (i != iMutiplier) {
-                    divs.add(n/i); //unsort insertion [1, 36, 2, 18, 3, 12, 4, 9, 6] // or use sort insertion [1, 2, 3, 4, 6, 9, 12, 18, 36] with below logic
-                    // if (divs.size()>1 && iMutiplier < divs.get(divs.size()/2 +1)) divs.add(divs.size()/2 +1, iMutiplier); else divs.add(iMutiplier);
+                if (i != iMutiplier) { // just to avoid square root number i.e 6 and we already know that iMultiplier is divisor as n%i == 0 because n/i is nothing but iMultiplier
+                    divs.add(divs.size()/2 +1, iMutiplier);
+                    ////PATTERM=> [1] iM=36 | [1,2,36] iM=16 | [1,2,3,18,36] iM=12 -- size is odd
                 }
+                    
+            }
+        }
+        return divs;
+
+    }
+
+
+    static List<Integer> getDivisorsWithSqrtLoopMoreConditions(int n) { // use getDivisorsWithSqrtLoop() instead of this
+        List<Integer> divs = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n%i == 0) {
+                // NORMAL ---------------
+                // divs.add(i); //unsort insertion [1, 36, 2, 18, 3, 12, 4, 9, 6] // or use sort insertion [1, 2, 3, 4, 6, 9, 12, 18, 36] with below logic by inserting the i in middle
+                if (divs.size()>1 && i < divs.get(divs.size()/2)) divs.add(divs.size()/2, i); else divs.add(i); // else conditon is corener case for '1' and so all other nums inserted in if condition // or  if (divs.size()>1 && i < divs.get(divs.size()/2) )
+                ////[] i=1 | [1,36] i=2 | [1,2,18,26] i=3 -- size is even
+
+                // COUNTERPART ---------------
+                int iMutiplier = n/i;
+                if (i != iMutiplier) { // just to avoid square root number i.e 6 and we already know that iMultiplier is divisor as n%i == 0 because n/i is nothing but iMultiplier
+                    // divs.add(n/i); //unsort insertion [1, 36, 2, 18, 3, 12, 4, 9, 6] // or use sort insertion [1, 2, 3, 4, 6, 9, 12, 18, 36] with below logic by inserting the iMultiplier in middle
+                    if (divs.size()>1 && iMutiplier < divs.get(divs.size()/2 +1)) divs.add(divs.size()/2 +1, iMutiplier); else divs.add(iMutiplier);  // --------- to place in the middle // else is corener case for 36 i.e self n           // or if(divs.size()>1) i.e for reading purpose gave 2 or if(divs.size()>2 && iMutiplier < divs.get(divs.size()/2 +1))
+                    ////[1] iM=36 | [1,2,36] iM=16 | [1,2,3,18,36] iM=12 -- size is odd
+                }
+                    
             }
         }
         return divs;
